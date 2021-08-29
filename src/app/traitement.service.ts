3fa28c0cde28b7a61732ba30876c266df6b24fd3
Amarmouhamed <amarmouhamed4@gmail.com>
2021-08-29 07:09:17 +0000

    0709

--- src/app/traitement.service.ts
+++ src/app/traitement.service.ts
@@ -5,52 +5,46 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TraitementService {
-  currentPage = true;
-  host = "localhost/backen_mbokk";
-  host_connexion = "http://192.168.1.9/genealogie_back";
-  utilisateur: any
-  liste = [
-    { prenom: "Aliou Diagne", nom: "Amar" },
-    { prenom: "Gora", nom: "Amar" },
-    { prenom: "Tala", nom: "Amar" },
-  ]
+  host_connexion="http://192.168.1.9/genealogie_back";
+  utilisateur:any
+  currentPage=true
   // liste de listes
-  arbre: any = [
+  arbre:any=[
     //une generation beaucoup plus ancienne
     [
-      { prenom: "Aliou (Baye Alla)", nom: "Amar" }
+      {prenom:"Aliou (Baye Alla)",nom:"Amar"}
     ],
     //une generation plus ancienne
     [
-      { prenom: "Aliou Diagne", nom: "Amar" },
-      { prenom: "Gora", nom: "Amar" },
-      { prenom: "Tala", nom: "Amar" },
+      {prenom:"Aliou Diagne",nom:"Amar"},
+      {prenom:"Gora",nom:"Amar"},
+      {prenom:"Tala",nom:"Amar"},
     ],
     //une generation
     [
-      { prenom: "Mouhamed", nom: "Amar" },
-      { prenom: "Modou", nom: "Amar" },
-      { prenom: "Khady", nom: "Amar" },
+      {prenom:"Mouhamed",nom:"Amar"},
+      {prenom:"Modou",nom:"Amar"},
+      {prenom:"Khady",nom:"Amar"},
     ]
   ]
-  constructor(public http: HttpClient) { }
+  constructor(public http:HttpClient) { }
   /*
     elle lance une reque post dont les infos sont dans un formdata
     et puis execute : 
         succes_callback en cas de succes
         erreur_callback en cas de succes
   */
-  lancer_requete_post(page: string, formData: any, succes_callback: Function, erreur_callback: Function) {
-    this.http.post(this.host_connexion + page, formData)
-      .subscribe((res: any) => {
-        console.log("succes: ", res)
-        succes_callback(res)
-      }, (err: any) => {
-        console.log("erreur: ", err)
+  lancer_requete_post(page:string,formData:any,succes_callback:Function,erreur_callback:Function){
+    this.http.post(this.host_connexion+page,formData)
+    .subscribe((res:any) => {
+         console.log("succes: ", res)
+          succes_callback(res)
+      }, (err:any) => {
+        console.log("erreur: ",err)
         erreur_callback(err)
-      });
+    });
   }
-  connecte_oui_non() {
+  connecte_oui_non(){
     if (this.utilisateur) {
       return true
     } else {
